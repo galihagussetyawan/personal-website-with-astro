e7163a258f1b3897753a532a1ac575fc7dea672a
Galih Agus Setyawan <galihdevelopment@gmail.com>
2022-10-14 23:01:24 +0700

    add page About

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), image(), sitemap()]
 });
