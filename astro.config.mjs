91b3e8a537d0da4bc27781ca0d9bfebaa78431ec
Galih Agus Setyawan <galihdevelopment@gmail.com>
2022-10-27 03:40:19 +0700

    add React component

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,13 @@ import sitemap from "@astrojs/sitemap";
 import compress from "astro-compress";
 
 // https://astro.build/config
+import prefetch from "@astrojs/prefetch";
+
+// https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://galihagussetyawan.xyz',
-  integrations: [tailwind(), image(), sitemap(), compress()]
+  integrations: [tailwind(), image(), sitemap(), compress(), prefetch(), react()]
 });
